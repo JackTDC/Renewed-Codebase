b1a036953cf378a92560ab3e2783585cf50a005a
Arpit <jackathu439@gmail.com>
2023-04-13 01:49:45 +0530

    first commit

---
+++ Number Guesser/Program(NEW).cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the number guesser!");
+
+        int min=0;
+        int max=0;
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            min = GetNumber("Enter the minimum number in the range: ");
+            max = GetNumber("Enter the maximum number in the range: ");
+
+            if (min > max)
+            {
+                Console.WriteLine("Minimum can't be larger than maximum. Please try again.");
+            }
+            else
+            {
+                validInput = true;
+            }
+        }
+
+        Console.WriteLine("Think of a number between {0} and {1}", min, max);
+
+        int guess = (min + max) / 2;
+
+        while (true)
+        {
+            Console.Write("Is your number {0}? (Y/H/L): ", guess);
+            char input = GetInput();
+
+            if (input == 'Y')
+            {
+                Console.WriteLine("I guessed your number!");
+                break;
+            }
+            else if (input == 'H')
+            {
+                min = guess + 1;
+            }
+            else if (input == 'L')
+            {
+                max = guess - 1;
+            }
+
+
+            guess = (min + max) / 2;
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+    static int GetNumber(string prompt)
+    {
+        int number;
+
+        while (true)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number))
+            {
+                return number;
+            }
+
+
+
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+
+    static char GetInput()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.Length == 1 && "YHL".Contains(input.ToUpper()))
+            {
+                return input.ToUpper()[0];
+            }
+
+            Console.WriteLine("Invalid input. Please enter Y, H, or L.");
+        }
+    }
+}
---
+++ Number Guesser/Program(OLD).cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("YOOO Welcome to number guesser");
+        Console.WriteLine("Think of a number from 1 to 1000");
+
+
+        int max = 1000;
+        int min = 1;
+        int avg = 500;
+
+        Console.Write("Is your number higher than 500 (Y/H/L) : ");
+        char inp = Convert.ToChar(Console.ReadLine());
+
+        while (inp != 'Y')
+        {
+            if (inp == 'H')
+            {
+                min = avg;
+                avg = (avg + max) / 2;
+
+            }
+            else if (inp == 'L')
+            {
+                max = avg;
+                avg = (avg + min) / 2;
+
+            }
+
+            Console.Write("Is your number higher than " + avg + " (Y/H/L) : ");
+            inp = Convert.ToChar(Console.ReadLine());
+
+        }
+
+        Console.WriteLine("HA! I guessed your number");
+    }
+}
